0ba5440ae35ba4fc4bb28396c35901c671cbf098
Diego Aldao <diego_ind@live.com.ar>
2025-09-21 14:35:15 -0300

    quitados los hovers innecesarios

--- components/GridIlustraciones/NombrePrenda.tsx
+++ components/GridIlustraciones/NombrePrenda.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export default function NombrePrenda({ nombre }: Props) {
   return (
-    <div className="relative  after:w-full after:absolute after:h-[1px] w-fit  hover:after:right-10px after:-bottom-[0px] after:bg-background right-[5px] top-[5px] capitalize h-fit font-medium ml-auto pt-2 pr-4 text-[10px] text-background md:text-xs lg:text-sm xl:text-base lg:pt-4 lg:pr-8 hover:after:-translate-x-2 after:delay-200 after:right-0 after:ease-custom after:transition-[opacity,translate] overflow-hidden pb-[1px]">
+    <div className="relative  after:w-full after:absolute after:h-[1px] w-fit after after:-bottom-[0px] after:bg-background right-[5px] top-[5px] capitalize h-fit font-medium ml-auto pt-2 pr-4 text-[10px] text-background md:text-xs lg:text-sm xl:text-base lg:pt-4 lg:pr-8 after:right-0 overflow-hidden pb-[1px]">
       <span>{nombre}</span>
     </div>
   );
